e1b713a61ee669e3e3ce976e01c5ffa0cc2b5cea
wenjunzheng <wenjunzheng@sohu-inc.com>
2021-12-15 23:12:09 +0800

    调整筛选框的交互

--- src/components/File.js
+++ src/components/File.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {PlusOutlined, FlagOutlined, CarryOutOutlined, SortAscendingOutlined} from "@ant-design/icons";
+import {PlusOutlined, FlagOutlined, CarryOutOutlined, SortAscendingOutlined, FilterOutlined} from "@ant-design/icons";
 import {Row, Col, Input, Button, Popover, List, Skeleton, Space} from "antd";
 import todoApi from "../http/todo";
 import "../assets/style/file.less"
@@ -11,48 +11,20 @@ const IconText = ({className = "", icon = null, text = ""}) => (
   </Space>
 );
 
-const sortPopContent = (
-  <div className="sort-opt-con">
-    <Button
-      type="text" className="sort-opt-con-item"
-      onClick={() => {
-        this.sortOptClick()
-      }}>
-      按标题降序
-    </Button>
-    <Button
-      type="text" className="sort-opt-con-item"
-      onClick={() => {
-        this.sortOptClick('deadline')
-      }}>
-      按时间降序
-    </Button>
-    <Button
-      type="text" className="sort-opt-con-item"
-      onClick={() => {
-        this.sortOptClick('status')
-      }}>
-      按状态降序
-    </Button>
-    <Button
-      type="text" className="sort-opt-con-item"
-      onClick={() => {
-        this.sortOptClick('priority')
-      }}>
-      按优先级降序
-    </Button>
-  </div>
-);
 
 class File extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       activeItem: {},
-      keywords: '',
-      from: props.state.from,
-      loading: false,
-      todoList: [],
+      filterForm: {
+        keywords: '',
+        sortBy  : '',
+        priority: 0,
+      },
+      from      : props.state.from,
+      loading   : false,
+      todoList  : [],
     }
   }
 
@@ -62,7 +34,7 @@ class File extends React.Component {
 
   loadtodoList() {
     this.setState({loading: true})
-    let params = {}
+    let params = this.state.filterForm
     todoApi.todoList(params).then(response => {
       if (response.code === 200) {
         this.setState({loading: false, todoList: response.data.list})
@@ -73,23 +45,91 @@ class File extends React.Component {
   }
 
   searchChange(e) {
-    var keywords = e.target.value
-    this.setState({keywords: keywords})
+    var filterForm      = this.state.filterForm
+    filterForm.keywords = e.target.value
+
+    this.setState({filterForm: filterForm})
+
     this.loadtodoList()
   }
 
-  sortOptClick() {
-    console.log('click')
+  sortOptClick(sortBy) {
+    var filterForm    = this.state.filterForm
+    filterForm.sortBy = sortBy
+
+    this.setState({filterForm: filterForm})
+
+    this.loadtodoList()
+  }
+
+  filterOptClick(filterType) {
+    var filterForm    = this.state.filterForm
+
+    if (filterType === 'priority') {
+      filterForm.priority = 3
+    }
+
+    this.setState({filterForm: filterForm})
+    this.loadtodoList()
   }
 
   itemClick(item) {
     this.setState({activeItem: item})
   }
 
+  filterPopContent() {
+    return (
+      <div className="filter-opt-con">
+        <Button
+          type="text" className="filter-opt-con-item"
+          onClick={() => {
+            this.filterOptClick('priority')
+          }}>
+          仅展示高优先级
+        </Button>
+      </div>
+    )
+  }
+
+  sortPopContent() {
+    return (
+      <div className="sort-opt-con">
+        <Button
+          type="text" className="sort-opt-con-item"
+          onClick={() => {
+            this.sortOptClick('title')
+          }}>
+          按标题降序
+        </Button>
+        <Button
+          type="text" className="sort-opt-con-item"
+          onClick={() => {
+            this.sortOptClick('deadline')
+          }}>
+          按时间降序
+        </Button>
+        <Button
+          type="text" className="sort-opt-con-item"
+          onClick={() => {
+            this.sortOptClick('status')
+          }}>
+          按状态降序
+        </Button>
+        <Button
+          type="text" className="sort-opt-con-item"
+          onClick={() => {
+            this.sortOptClick('priority')
+          }}>
+          按优先级降序
+        </Button>
+      </div>
+    )
+  }
+
   getListActions(item) {
     var currentDate = Date.now()
-    var expireDate = new Date(item.deadline).getTime()
-    var remainDate = expireDate - currentDate
+    var expireDate  = new Date(item.deadline).getTime()
+    var remainDate  = expireDate - currentDate
 
     var deadlineClassName = ''
     if (remainDate < 24 * 60 * 60 * 1000) {
@@ -104,20 +144,20 @@ class File extends React.Component {
       deadlineClassName = 'primary'
     }
 
-    var priorityText = '无'
+    var priorityText      = '无'
     var priorityClassName = ''
     if (item.priority === 3) {
-      priorityText = '高'
+      priorityText      = '高'
       priorityClassName = 'danger'
     }
 
     if (item.priority === 2) {
-      priorityText = '中'
+      priorityText      = '中'
       priorityClassName = 'warning'
     }
 
     if (item.priority === 1) {
-      priorityText = '低'
+      priorityText      = '低'
       priorityClassName = 'primary'
     }
 
@@ -147,10 +187,6 @@ class File extends React.Component {
     ]
   }
 
-  getPriority(priority) {
-
-  }
-
   render() {
     return (
       <div>
@@ -165,9 +201,22 @@ class File extends React.Component {
                 </div>
               </Col>
               <Col span={4} style={{paddingLeft: '10px'}}>
-                <Popover content={sortPopContent} trigger="click">
-                  <Button type="text">
+                <Popover
+                  placement="bottomLeft"
+                  title="选择排序方式"
+                  content={this.sortPopContent()}
+                  trigger="click">
+                  <Button type="text" className="filter-form-opt">
                     <SortAscendingOutlined/>
+                  </Button>
+                </Popover>
+                <Popover
+                  placement="bottomLeft"
+                  title="过滤展示内容"
+                  content={this.filterPopContent()}
+                  trigger="click">
+                  <Button type="text" className="filter-form-opt">
+                    <FilterOutlined />
                   </Button>
                 </Popover>
               </Col>
