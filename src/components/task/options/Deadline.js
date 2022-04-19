44ad9a0cbdd48002aceb2df9bc2d06fbf5ed3e68
wenjun zheng <wenjunzheng@sohu-inc.com>
2022-04-19 11:12:20 +0800

    任务截至时间操作项

--- src/components/task/options/Deadline.js
+++ src/components/task/options/Deadline.js
@@ -1,8 +1,7 @@
 import React from "react";
-import {CarryOutOutlined} from "@ant-design/icons";
 import 'moment/locale/zh-cn';
 import locale from 'antd/es/date-picker/locale/zh_CN';
-import {DatePicker} from "antd";
+import {DatePicker, Button} from "antd";
 import moment from "moment";
 import "../../../assets/style/opt.less"
 
@@ -10,6 +9,7 @@ class Deadline extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div style={{display: 'flex'}}>
@@ -25,10 +25,46 @@ class Deadline extends React.Component {
           value={moment(this.props.currentTask.deadline)}
           inputReadOnly={true}
           placeholder="截止时间"
+          showToday={false}
+          renderExtraFooter={() =>
+            <div style={{width: '100%;', textAlign: 'center', padding: '10px 0px'}}>
+              <Button type="text" onClick={() => this.quickDate(0)}>今天</Button>
+              <Button type="text" onClick={() => this.quickDate(1)}>明天</Button>
+              <Button type="text" onClick={() => this.quickDate(2)}>下周</Button>
+              <Button type="text" onClick={() => this.quickDate(3)}>下月</Button>
+            </div>}
           allowClear={false}
           suffixIcon={null}/>
       </div>
     )
+  }
+
+  quickDate(type = 0) {
+    var date = new Date()
+    if (type === 1) {
+      var date = moment(date).add(1, 'days').format("YYYY-MM-DD")
+      this.deadlineChange(date)
+      return
+    }
+
+    if (type === 2) {
+      var dow = moment(date).day();
+      var monday = moment(date).add(-(dow-1), 'days').format('YYYY-MM-DD')//本周一
+      var nextMonday = moment(monday).add(7, 'days').format('YYYY-MM-DD')//下周一
+      this.deadlineChange(nextMonday)
+      return
+    }
+
+    if (type === 3) {
+      var nextMon = moment(date).add(1, 'months').format("YYYY-MM")
+      var date = moment(nextMon).format('YYYY-MM-DD')
+      this.deadlineChange(date)
+      return
+    }
+
+    var date = moment(date).format("YYYY-MM-DD")
+    this.deadlineChange(date)
+    return
   }
 
   deadlineChange(val) {
