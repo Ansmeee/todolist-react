00406709c4f9faa04dc26301b2b3a440e991ced4
wenjun zheng <wenjunzheng@sohu-inc.com>
2022-04-22 15:22:02 +0800

    调整单个任务展示的逻辑

--- src/views/All.js
+++ src/views/All.js
@@ -1,10 +1,12 @@
 import React from "react";
 import File from "../components/File";
 import {Empty} from "antd";
+import browserHistory from "react-router/lib/browserHistory";
 class All extends React.Component {
   render() {
+    var sid = (browserHistory.getCurrentLocation().query && browserHistory.getCurrentLocation().query.s_id) || ''
     return (
-      <File state={{from: 'all'}} emptyText={<Empty description="好记性不如做计划" />}></File>
+      <File state={{from: 'all', sid: sid}} emptyText={<Empty description="好记性不如做计划" />}></File>
     )
   }
 }
